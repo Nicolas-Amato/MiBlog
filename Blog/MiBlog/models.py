626b4d5c4bd06e359f6bf4aca2bbdc4d0feaba68
NicolasAmato <nicolasamato@outlook.com>
2022-12-13 09:16:21 -0300

    actual

--- Blog/MiBlog/models.py
+++ Blog/MiBlog/models.py
@@ -3,12 +3,19 @@ from datetime import date, datetime
 from django.contrib.auth.models import User
 
 class autor(models.Model):
-    nombre = models.CharField(max_length = 40)
-    titulo = models.CharField(max_length = 40)
-    email = models.EmailField()
-    fecha = models.DateField(auto_now_add=True)
-    subtitulo = models.CharField(max_length=100)
+
+    nombre = models.CharField(max_length=40)
+
+    titulo = models.CharField(max_length=40)
+
+    email = models.EmailField(default='test@test.com')
+
+    fecha = models.DateField(default=datetime.now)
+
+    subtitulo = models.CharField(max_length=100,blank=True)
+
     post = models.TextField("")
+
     def __str__(self):
         return f' {self.nombre} {self.titulo}'
 
@@ -17,5 +24,4 @@ class avatar(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     imagen = models.ImageField(upload_to='images/', null=True, blank=True)
-    
     
